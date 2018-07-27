292f587b0452fa27040cb228e85ada1a0eb4ca7d
tagasouichiro <stitow14@gmail.com>
2018-07-27 23:06:36 +0900

    Fix wasm

--- gowasm/main.go
+++ gowasm/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"syscall/js"
 )
 
 func main() {
+
+	// Execute JS function
 	alert := js.Global().Get("alert")
 	alert.Invoke("Hello Wasm!!")
+
+	// Parameter from JS
+	cb := js.NewCallback(callBack)
+	defer cb.Release()
+	js.Global().Get("document").Call("getElementById", "gocall").Call("addEventListener", "click", cb)
+}
+
+func callBack(args []js.Value) {
+	fmt.Println(args[0])
+	fmt.Println("Hi Callback")
 }
